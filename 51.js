d74ef6a6542226795b1cfcba100d7c289f11be49
Malena Rey <malerey@male.5d640511d691.sn.mynetname.net>
2019-12-06 21:54:00 -0300

    first commit

---
+++ 51.js
@@ -0,0 +1,31 @@
+
+let password = "lili12"
+
+
+const numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+// la funcion que chequea el largo es mas o menos asi:
+if (password.length >= 6) {
+}
+
+// la funcion que chequea si tiene numeros es mas o menos asi:
+for (let i = 0; i < password.length; i++) {
+  if (numeros.indexOf(password[i]) != -1) {
+    //
+  }
+
+  // la funcion que chequea si tiene letras  es mas o menos asi:
+  for (let i = 0; i < password.length; i++) {
+    if (letras.indexOf(password[i]) != -1) {
+      //
+}
+
+
+
+
+
+
+
+
+
